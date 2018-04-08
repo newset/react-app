58e265c22586355527457cd463127873b7aacfdf
jie <luojie@doctorwork.com>
2018-04-08 10:50:34 +0800

    添加其他模块

---
+++ src/screens/HealthPlan.js
@@ -0,0 +1,27 @@
+import Component from "../components/component.react";
+import React from "react-native";
+import { msg } from "../intl/store";
+import { View, Text } from "react-native";
+
+import * as style from "../app/app.style";
+
+class Home extends Component {
+	render() {
+		const { navigation } = this.props;
+		return (
+			<View style={style.containerWithNavbar}>
+				<View style={[style.centeredView, style.paddingBottom]}>
+					<Text style={[style.centered, style.paragraph]}>
+						{msg("home.text")}
+					</Text>
+				</View>
+			</View>
+		);
+	}
+}
+
+Home.propTypes = {
+	navigation: React.PropTypes.object.isRequired
+};
+
+export default Home;
