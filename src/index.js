d8c9527ca0221f0e870a5bd5cd8fc6f01386930f
“lihuanji” <158757774@qq.com>
2018-04-10 15:10:33 +0800

    取消eslint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import createStore from './redux/store';
@@ -13,4 +13,5 @@ ReactDOM.render(
     </Provider>,
     document.getElementById("root")
 );
+
 registerServiceWorker();
