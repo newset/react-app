fe6fcdaab8e6c40d24a3298ae4ab817324e38310
“lihuanji” <158757774@qq.com>
2018-04-10 14:31:24 +0800

    正常跑start

--- src/redux/reducers/info.js
+++ src/redux/reducers/info.js
@@ -1,6 +1,6 @@
 import ActionType from '../actions/ActionType';
 
-export default (state, action) => {
+export default (state = {test: 1}, action) => {
     switch (action.type) {
         case ActionType.GET_INFO:
             return action.payload;
