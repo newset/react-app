12942af01c20df81e54957a4c3dd43c74eaea173
“lihuanji” <158757774@qq.com>
2018-04-10 20:03:01 +0800

    加上路由

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import createStore from './redux/store';
+import routes from './route';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore();
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <div>
+            <App />
+            <BrowserRouter>
+                <div>{routes}</div>
+            </BrowserRouter>
+        </div>
     </Provider>,
     document.getElementById('root')
 );
