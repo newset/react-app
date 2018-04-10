92a18b61d2954e14ca06bf643b548911c8d17c64
“lihuanji” <158757774@qq.com>
2018-04-10 10:44:09 +0800

    增加tslint依赖，saga

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
 import App from "./App";
+import createStore from './redux/store';
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById("root")
+);
 registerServiceWorker();
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 import rootReducer from '../reducers/index';
+import saga from './saga';
 
-const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
+const createStoreWithMiddleware = applyMiddleware(createSagaMiddleware)(createStore);
 
 export default (initialState) => {
     const store = createStoreWithMiddleware(rootReducer, initialState);
+    sagaMiddleware.run(mySaga);
     return store;
 };
