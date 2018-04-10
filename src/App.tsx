c534b38487ddc799dac0095bc66a05631dd1bf2c
“lihuanji” <158757774@qq.com>
2018-04-10 14:10:47 +0800

    增加eslint 修改一些lint配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import React, { Component } from "react";
-import logo from "./assets/logo.svg";
-import "./styles/main.styl";
+import React, { Component } from 'react';
+import './styles/main.styl';
+
+const logo = require('./assets/logo.svg') as string;
 
 class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
-			</div>
-		);
-	}
+    public render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from 'react-redux';
-import App from "./App";
+import App from './App';
 import createStore from './redux/store';
-import registerServiceWorker from "./registerServiceWorker";
+import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore();
 
