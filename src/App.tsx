bd1d84b7703df64ae0e78a63b929aeb4664ebd45
“lihuanji” <158757774@qq.com>
2018-04-10 18:59:01 +0800

    增加依赖

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import React, { Component } from "react";
-import "./styles/main.styl";
+import React, { Component } from 'react';
+import './styles/main.styl';
 
-const logo = require("./assets/logo.svg") as string;
+const logo = require('./assets/logo.svg') as string;
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to
+                    reload.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
