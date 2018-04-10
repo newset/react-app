738d63079cab88d86011ffed28b61f992a83e3db
“lihuanji” <158757774@qq.com>
2018-04-10 18:56:08 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import App from "./App";
-import createStore from "./redux/store";
-import registerServiceWorker from "./registerServiceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import createStore from './redux/store';
+import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore();
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
 
 registerServiceWorker();
