e1a441d82c722620fdc425c27f32a9bbcc0ae4c7
jie <luojie@doctorwork.com>
2018-04-12 14:18:41 +0800

    去掉rewire

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-/* config-overrides.js */
-const loaders = require('./config/loaders');
-
-module.exports = function override(config, env) {
-    // 添加 loaders
-    config = loaders(config, env);
-
-    return config;
-};
--- loaders/loaders.js
+++
@@ -1,8 +0,0 @@
-const rewireTypescript = require('react-app-rewire-typescript');
-const rewirestylus = require('./stylus');
-const eslintLoader = require('./eslint');
-const { compose } = require('react-app-rewired');
-
-module.exports = (config, env) => {
-    return compose(rewirestylus, rewireTypescript, eslintLoader)(config, env);
-};
